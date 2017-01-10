e234e80d5356cb38c6ed5e3382c474ae5edee1d9
jayess <jayess@jayess.za.net>
2017-01-10 16:23:56 +0200

    Fix weird search behaviour.

--- src/components/bsp/EntitySearch.js
+++ src/components/bsp/EntitySearch.js
@@ -6,44 +6,18 @@ import {EntityFilterActions} from '../../actions'
 export default class EntitySearch extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {value: null}
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    const f = nextProps.textFilter
-    if (f !== this.props.textFilter && f !== null) {
-      this.refs.input.getDOMNode().value = f
-    }
-  }
-
-  startTimeout() {
-    this.triggerSearch()
-    this.clearTimeout()
-    this.timeoutID = window.setTimeout(this.triggerSearch, 0)
-  }
-
-  clearTimeout() {
-    if (this.timeoutID) {
-      window.clearTimeout(this.timeoutID)
-      delete this.timeoutID
-    }
-  }
-
-  triggerSearch() {
-    EntityFilterActions.setTextFilter(this.state.value)
   }
 
   onChange(e) {
-    this.setState({value: e.target.value})
-    this.startTimeout()
+    EntityFilterActions.setTextFilter(e.target.value)
   }
-
 
   render() {
     return <input ref="input"
                   className="form-control"
                   type="text"
                   onChange={this.onChange.bind(this)}
-                  placeholder="Search entities" />
+                  placeholder="Search entities"
+                  value={this.props.textFilter} />
   }
 }
